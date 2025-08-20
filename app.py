7748914a4965a43b11246b8412eba5fa69b34fff
Embenzange <berthakasiera@gmail.com>
2025-08-20 17:45:07 +0300

    Updated project files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -46,4 +47,5 @@ def predict():
     return render_template('index.html')    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT dynamically
+    app.run(host="0.0.0.0", port=port, debug=False)
